20721543020b60e399ccacd190cc18ae160156c7
Xandra Campo <xkmpera@gmail.com>
2024-07-11 17:01:40 +0200

    Document evaluation.py, add log scale option in plots, add function to read data set's output files.

--- evaluation.py
+++ evaluation.py
@@ -1,16 +1,29 @@
+# Import necessary libraries
+import os
+import re
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# Define paths for data set and figures
+data_set_path = 'assets/data_set/'
+figures_path = 'assets/figures/'
+
 
 def plot_histograms(n):
-    df = pd.read_csv(f'output/out_{n}.csv', index_col=0)
+    # Read the CSV file for the given number of iterations
+    df = pd.read_csv(data_set_path + f'out_{n}.csv', index_col=0)
     iterations = df.loc[0:n - 1]
-    print(f'Minimum and maximum filter thickness (mm):')
-    print(f'Al: ({iterations['Al (mm)'].min():.2f}, {iterations['Al (mm)'].max():.2f})')
-    print(f'Cu: ({iterations['Cu (mm)'].min():.2f}, {iterations['Cu (mm)'].max():.2f})')
 
+    # Print min and max filter thickness for Al and Cu
+    print(f'{n} iterations: min and max filter thickness (mm):')
+    print(f'Al: ({iterations["Al (mm)"].min():.2f}, {iterations["Al (mm)"].max():.2f})')
+    print(f'Cu: ({iterations["Cu (mm)"].min():.2f}, {iterations["Cu (mm)"].max():.2f})')
+
+    # Create subplots for histograms
     fig, axs = plt.subplots(3, 4, figsize=(12.8, 9.6))
+
+    # Plot histograms for various parameters
     axs[0, 0].hist(iterations['kVp (kV)'])
     axs[0, 1].hist(iterations['th (deg)'])
     axs[0, 2].hist(iterations['Air (mm)'])
@@ -23,6 +36,8 @@ def plot_histograms(n):
     axs[2, 1].hist(iterations['Mean energy (keV)'])
     axs[2, 2].hist(iterations['Air kerma (uGy)'])
     axs[2, 3].hist(iterations['Mean conv. coeff. (Sv/Gy)'])
+
+    # Set labels for each subplot
     axs[0, 0].set_xlabel('kVp (kV)')
     axs[0, 1].set_xlabel('th (deg)')
     axs[0, 2].set_xlabel('Air (mm)')
@@ -35,102 +50,162 @@ def plot_histograms(n):
     axs[2, 1].set_xlabel('Mean energy (keV)')
     axs[2, 2].set_xlabel('Air kerma (uGy)')
     axs[2, 3].set_xlabel('Mean conv. coeff. (Sv/Gy)')
+
+    # Set the title for the entire figure
     plt.suptitle(f'Reference case with {n} iterations')
-    plt.savefig(f'output/histogram_{n}.png')
+
+    # Save the figure to a file and close it
+    plt.savefig(figures_path + f'histogram_{n}.png')
     plt.close()
     return
 
 
-def plot_execution_time(iterations, times, suffix):
+def plot_execution_time(iterations, times, suffix, log=False):
+    # Convert times to minutes
     t_m = times / 60
+
     # Fit a straight line to the data
     coefficients = np.polyfit(iterations, t_m, 1)
     slope, intercept = coefficients
-    # Calculate the fitted y values
-    y_fit = slope * iterations + intercept
+    y_fit = slope * iterations + intercept  # Calculate the fitted y values
+
     # Equation of the fitted line
     equation_text = f't (min) = {slope:.2f}n + {intercept:.2f}'
-    # Plot the original data, the fitted line and the fitted equation
+
+    # Plot the original data and the fitted line
     plt.scatter(iterations, t_m, label='Data')
     plt.plot(iterations, y_fit, label='Fit')
     plt.xlabel('Number of iterations')
     plt.ylabel('Execution time (min)')
-    # plt.xscale('log')
     plt.legend()
     plt.text(0.05, 0.95, equation_text, transform=plt.gca().transAxes)
-    # plt.show()
-    plt.savefig(f'output/execution_time{suffix}.png')
+
+    # Apply log scale if specified
+    if log:
+        plt.xscale('log')
+        plt.savefig(figures_path + f'execution_time_log.png')
+    else:
+        plt.savefig(figures_path + f'execution_time.png')
+
+    # Close the plot
     plt.close()
     return
 
 
-def plot_values_and_uncertainties(iterations):
+def plot_values_and_uncertainties(iterations, log=False):
     means = []
     relative_uncertainties = []
+
+    # Read mean values and uncertainties for each iteration
     for n in iterations:
-        df = pd.read_csv(f'output/out_{n}.csv', index_col=0)
+        df = pd.read_csv(data_set_path + f'out_{n}.csv', index_col=0)
         df = df.set_index('#')
         means.append(df.loc['Mean', :])
         relative_uncertainties.append(df.loc['Relative uncertainty', :])
 
-    means = pd.DataFrame(dict(zip(iterations, means)))
-    means = means.T
+    # Convert lists to DataFrames
+    means = pd.DataFrame(dict(zip(iterations, means))).T
     fig, axs = plt.subplots(nrows=3, ncols=4, figsize=(19.2, 14.4))
-    means['kVp (kV)'].plot(ax=axs[0, 0], title='kVp (kV)', marker='o')
-    means['th (deg)'].plot(ax=axs[0, 1], title='th (deg)', marker='o')
-    means['Air (mm)'].plot(ax=axs[0, 2], title='Air (mm)', marker='o')
-    means['Al (mm)'].plot(ax=axs[0, 3], title='Al (mm)', marker='o')
-    means['Cu (mm)'].plot(ax=axs[1, 0], title='Cu (mm)', marker='o')
-    means['HVL1 Al (mm)'].plot(ax=axs[1, 1], title='HVL1 Al (mm)', marker='o')
-    means['HVL2 Al (mm)'].plot(ax=axs[1, 2], title='HVL2 Al (mm)', marker='o')
-    means['HVL1 Cu (mm)'].plot(ax=axs[1, 3], title='HVL1 Cu (mm)', marker='o')
-    means['HVL2 Cu (mm)'].plot(ax=axs[2, 0], title='HVL2 Cu (mm)', marker='o')
-    means['Mean energy (keV)'].plot(ax=axs[2, 1], title='Mean energy (keV)', marker='o')
-    means['Air kerma (uGy)'].plot(ax=axs[2, 2], title='Air kerma (uGy)', marker='o')
-    means['Mean conv. coeff. (Sv/Gy)'].plot(ax=axs[2, 3], title='Mean conv. coeff. (Sv/Gy)', marker='o')
-    plt.suptitle(f'Mean values of input and output variables in terms of the number of iterations')
-    # plt.show()
-    plt.savefig(f'output/mean_values.png')
+
+    # Plot mean values for various parameters
+    means['kVp (kV)'].plot(ax=axs[0, 0], title='kVp (kV)', marker='o', logx=log)
+    means['th (deg)'].plot(ax=axs[0, 1], title='th (deg)', marker='o', logx=log)
+    means['Air (mm)'].plot(ax=axs[0, 2], title='Air (mm)', marker='o', logx=log)
+    means['Al (mm)'].plot(ax=axs[0, 3], title='Al (mm)', marker='o', logx=log)
+    means['Cu (mm)'].plot(ax=axs[1, 0], title='Cu (mm)', marker='o', logx=log)
+    means['HVL1 Al (mm)'].plot(ax=axs[1, 1], title='HVL1 Al (mm)', marker='o', logx=log)
+    means['HVL2 Al (mm)'].plot(ax=axs[1, 2], title='HVL2 Al (mm)', marker='o', logx=log)
+    means['HVL1 Cu (mm)'].plot(ax=axs[1, 3], title='HVL1 Cu (mm)', marker='o', logx=log)
+    means['HVL2 Cu (mm)'].plot(ax=axs[2, 0], title='HVL2 Cu (mm)', marker='o', logx=log)
+    means['Mean energy (keV)'].plot(ax=axs[2, 1], title='Mean energy (keV)', marker='o', logx=log)
+    means['Air kerma (uGy)'].plot(ax=axs[2, 2], title='Air kerma (uGy)', marker='o', logx=log)
+    means['Mean conv. coeff. (Sv/Gy)'].plot(ax=axs[2, 3], title='Mean conv. coeff. (Sv/Gy)', marker='o', logx=log)
+
+    # Save the plot with log scale if specified
+    if log:
+        plt.savefig(figures_path + f'mean_values_log.png')
+    else:
+        plt.savefig(figures_path + f'mean_values.png')
     plt.close()
 
-    relative_uncertainties = pd.DataFrame(dict(zip(iterations, relative_uncertainties)))
-    relative_uncertainties = relative_uncertainties.T
+    # Convert lists to DataFrames for relative uncertainties
+    relative_uncertainties = pd.DataFrame(dict(zip(iterations, relative_uncertainties))).T
     fig, axs = plt.subplots(nrows=3, ncols=4, figsize=(19.2, 14.4))
-    relative_uncertainties['kVp (kV)'].plot(ax=axs[0, 0], title='kVp (kV)', marker='o')
-    relative_uncertainties['th (deg)'].plot(ax=axs[0, 1], title='th (deg)', marker='o')
-    relative_uncertainties['Air (mm)'].plot(ax=axs[0, 2], title='Air (mm)', marker='o')
-    relative_uncertainties['Al (mm)'].plot(ax=axs[0, 3], title='Al (mm)', marker='o')
-    relative_uncertainties['Cu (mm)'].plot(ax=axs[1, 0], title='Cu (mm)', marker='o')
-    relative_uncertainties['HVL1 Al (mm)'].plot(ax=axs[1, 1], title='HVL1 Al (mm)', marker='o')
-    relative_uncertainties['HVL2 Al (mm)'].plot(ax=axs[1, 2], title='HVL2 Al (mm)', marker='o')
-    relative_uncertainties['HVL1 Cu (mm)'].plot(ax=axs[1, 3], title='HVL1 Cu (mm)', marker='o')
-    relative_uncertainties['HVL2 Cu (mm)'].plot(ax=axs[2, 0], title='HVL2 Cu (mm)', marker='o')
-    relative_uncertainties['Mean energy (keV)'].plot(ax=axs[2, 1], title='Mean energy (keV)', marker='o')
-    relative_uncertainties['Air kerma (uGy)'].plot(ax=axs[2, 2], title='Air kerma (uGy)', marker='o')
+
+    # Plot relative uncertainties for various parameters
+    relative_uncertainties['kVp (kV)'].plot(ax=axs[0, 0], title='kVp (kV)', marker='o', logx=log)
+    relative_uncertainties['th (deg)'].plot(ax=axs[0, 1], title='th (deg)', marker='o', logx=log)
+    relative_uncertainties['Air (mm)'].plot(ax=axs[0, 2], title='Air (mm)', marker='o', logx=log)
+    relative_uncertainties['Al (mm)'].plot(ax=axs[0, 3], title='Al (mm)', marker='o', logx=log)
+    relative_uncertainties['Cu (mm)'].plot(ax=axs[1, 0], title='Cu (mm)', marker='o', logx=log)
+    relative_uncertainties['HVL1 Al (mm)'].plot(ax=axs[1, 1], title='HVL1 Al (mm)', marker='o', logx=log)
+    relative_uncertainties['HVL2 Al (mm)'].plot(ax=axs[1, 2], title='HVL2 Al (mm)', marker='o', logx=log)
+    relative_uncertainties['HVL1 Cu (mm)'].plot(ax=axs[1, 3], title='HVL1 Cu (mm)', marker='o', logx=log)
+    relative_uncertainties['HVL2 Cu (mm)'].plot(ax=axs[2, 0], title='HVL2 Cu (mm)', marker='o', logx=log)
+    relative_uncertainties['Mean energy (keV)'].plot(ax=axs[2, 1], title='Mean energy (keV)', marker='o', logx=log)
+    relative_uncertainties['Air kerma (uGy)'].plot(ax=axs[2, 2], title='Air kerma (uGy)', marker='o', logx=log)
     relative_uncertainties['Mean conv. coeff. (Sv/Gy)'].plot(ax=axs[2, 3], title='Mean conv. coeff. (Sv/Gy)',
-                                                             marker='o')
-    plt.suptitle(f'Relative uncertainty of input and output variables in terms of the number of iterations')
-    # plt.show()
-    plt.savefig(f'output/relative_uncertainties.png')
+                                                             marker='o', logx=log)
+
+    # Save the plot with log scale if specified
+    if log:
+        plt.xscale('log')
+        plt.savefig(figures_path + f'relative_uncertainties_log.png')
+    else:
+        plt.savefig(figures_path + f'relative_uncertainties.png')
     plt.close()
     return
 
 
-if __name__ == "__main__":
-    iterations = [10, 50, 100, 500, 1000, 5000]
+def extract_numbers_from_filenames(directory):
+    # List all files in the given directory
+    files = os.listdir(directory)
 
-    # Probability distribution of input and output variables
+    # Define the regular expression pattern to match 'out_<number>.csv'
+    pattern = re.compile(r'^out_(\d+)\.csv$')
+
+    # List to store the extracted numbers
+    numbers = []
+
+    # Iterate over the files and apply the regex pattern
+    for file in files:
+        match = pattern.match(file)
+        if match:
+            # Extract the number and add it to the list
+            numbers.append(int(match.group(1)))
+
+    # Sort the numbers in ascending order
+    numbers.sort()
+
+    return numbers
+
+
+def main():
+    # Retrieve iteration numbers from output files
+    print('Retrieve iterations from output files.')
+    iterations = extract_numbers_from_filenames(data_set_path)
+    print(f'Retrieved {len(iterations)} cases with iteration numbers: {iterations}.')
+    print()
+
+    # Plot histograms for input and output variables
+    print('Plot probability distribution of input and output variables.')
     for i in iterations:
         plot_histograms(n=i)
+    print()
 
-    # Execution time
-    # iterations = np.array([10, 50, 100, 500, 1000])
-    # times = np.array([7.773966312408447, 38.55954885482788, 81.8867039680481, 412.0777871608734, 953.7109704017639])
-    # suffix = '_first_run'
-    times = np.array([8.112820148468018, 38.74364185333252, 74.59362983703613, 371.9314422607422, 1380.4966669082642,
-                      8006.918867588043])
-    suffix = '_second_run'
-    plot_execution_time(np.array(iterations), times, suffix)
+    # Plot execution time against the number of iterations
+    print('Plot execution time of one case in terms of the iteration number.')
+    df = pd.read_csv('assets/data_set/execution_times.csv', index_col=0)
+    plot_execution_time(df['Iterations'], df['Execution time (s)'], '_third_run')
+    plot_execution_time(df['Iterations'], df['Execution time (s)'], '_third_run', log=True)
+    print()
 
-    # Quantities mean values
+    # Plot mean values and uncertainties of input and output variables
+    print('Plot mean values and uncertainties of input and output variables in terms of the iteration number.')
     plot_values_and_uncertainties(iterations)
+    plot_values_and_uncertainties(iterations, log=True)
+
+
+# Run the main function if the script is executed
+if __name__ == "__main__":
+    main()
